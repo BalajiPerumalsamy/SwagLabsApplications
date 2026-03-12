387cabe238d43cb577a8f1c78e71035ab5b5cdcf
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-12 19:11:43 +0530

    New Test case Add

---
+++ src/test/java/com/negativeTests/InvalidLoginPageTest.java
@@ -0,0 +1,51 @@
+package com.negativeTests;
+
+import com.swagLabsPages.BasePage;
+import com.swagLabsPages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class InvalidLoginPageTest
+{
+    public BasePage basePage;
+    public LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUp()
+    {
+        basePage=new BasePage();
+        basePage.navigateToApplication();
+    }
+
+    @Test
+    public void verifyInvalidUserLogin()
+    {
+        loginPage=new LoginPage();
+        loginPage.login(basePage.properties.getProperty("invalidUserName"),basePage.properties.getProperty("invalidPassword"));
+        Assert.assertTrue(loginPage.verifyErrorMessage1().isDisplayed(),"Couldn't show error message");
+    }
+
+    @Test
+    public void verifyEmptyFieldLogin()
+    {
+        loginPage=new LoginPage();
+        loginPage.emptyFieldLogin();
+        Assert.assertTrue(loginPage.verifyErrorMessage2().isDisplayed(),"Couldn't show error message");
+    }
+
+    @Test
+    public void verifyLockedUserLogin()
+    {
+        loginPage=new LoginPage();
+        loginPage.login(basePage.properties.getProperty("lockedUserName"),basePage.properties.getProperty("password"));
+        Assert.assertTrue(loginPage.verifyErrorMessage3().isDisplayed(),"Couldn't show error message");
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {
+        basePage.quitApplications();
+    }
+}

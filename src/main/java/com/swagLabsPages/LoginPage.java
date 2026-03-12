4670e2f7278beab9c2948f0b514bdcd40e8eca19
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-12 19:12:40 +0530

    Code Modified

--- src/main/java/com/swagLabsPages/LoginPage.java
+++ src/main/java/com/swagLabsPages/LoginPage.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.WebElement;
 
 public class LoginPage extends BasePage
 {
-    By userName=By.xpath("//input[@name='user-name']");
-    By password=By.xpath("//input[@name='password']");
-    By loginButton=By.xpath("//input[@name='login-button']");
-    By productsList=By.xpath("//span[text()='Products']");
+    public By userName=By.xpath("//input[@name='user-name']");
+    public By password=By.xpath("//input[@name='password']");
+    public By loginButton=By.xpath("//input[@name='login-button']");
+    public By productsList=By.xpath("//span[text()='Products']");
+    public By errorMessage1=By.xpath("//h3[text()='Epic sadface: Username and password do not match any user in this service']");
+    public By errorMessage2=By.xpath("//h3[text()='Epic sadface: Username is required']");
+    public By errorMessage3=By.xpath("//h3[text()='Epic sadface: Sorry, this user has been locked out.']");
 
     public WebElement enterUserName()
     {
@@ -30,10 +33,30 @@ public class LoginPage extends BasePage
         return driver.findElement(productsList);
     }
 
+    public WebElement verifyErrorMessage1()
+    {
+        return driver.findElement(errorMessage1);
+    }
+
+    public WebElement verifyErrorMessage2()
+    {
+        return driver.findElement(errorMessage2);
+    }
+
+    public WebElement verifyErrorMessage3()
+    {
+        return driver.findElement(errorMessage3);
+    }
+
     public void login(String userName,String password)
     {
         writeText(userName,enterUserName());
         writeText(password,enterPassword());
+        clickButton(clickLoginButton());
+    }
+
+    public void emptyFieldLogin()
+    {
         clickButton(clickLoginButton());
     }
 

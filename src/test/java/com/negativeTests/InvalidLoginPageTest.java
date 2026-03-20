58e22a999a96e65b2ff2543738279fe0fc88efd9
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-03-20 12:28:15 +0530

    Code Modified

--- src/test/java/com/negativeTests/InvalidLoginPageTest.java
+++ src/test/java/com/negativeTests/InvalidLoginPageTest.java
@@ -1,12 +1,14 @@
 package com.negativeTests;
 
+import com.Listeners.MyListener;
 import com.swagLabsPages.BasePage;
 import com.swagLabsPages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
+@Listeners(MyListener.class)
 public class InvalidLoginPageTest
 {
     public BasePage basePage;
